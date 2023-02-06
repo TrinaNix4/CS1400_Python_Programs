90bf623456152e55c438a03010017e43c92a7285
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-06 14:46:19 -0700

    mad lib fixed formatting and syntax errors

--- MadLibAboutMe.py
+++ MadLibAboutMe.py
@@ -8,3 +8,8 @@ personalityTrait1 = input("Enter a personality trait: ")
 personalityTrait2 = input("Enter another personality trait: ")
 philosophy = input("Enter a philosophy: ")
 feeling = input("Enter a feeling: ")
+
+
+# print to the screen the paragraph with the user variables in their respective places
+print("Hello, my name is " + name + ". One thing I would like for you to know about me is when I am not programing I love to " + hobby + ". I also want you to know that I am a " + adjective + " student who loves " + love + " but absolutely hates " + hate +
+      "! The best way to describe my personality is " + personalityTrait1 + " and " + personalityTrait2 + ". My personal philosophy or a strong belief that I have that guides me through my life is " + philosophy + ". I am excited to learn about programming!")
