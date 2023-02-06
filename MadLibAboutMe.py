84c955f1a4667b31766a66aabeefc050652bc430
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-06 14:56:07 -0700

    fixed typo

--- MadLibAboutMe.py
+++ MadLibAboutMe.py
@@ -1,4 +1,4 @@
-# ask for user input and save to variables
+# ask for user input and save to named variables
 name = input("Enter your first and last name: ")
 hobby = input("Enter a favorite hobby: ")
 adjective = input("Enter an adjective: ")
@@ -10,6 +10,6 @@ philosophy = input("Enter a philosophy: ")
 feeling = input("Enter a feeling: ")
 
 
-# print to the screen the paragraph with the user variables in their respective places
-print("Hello, my name is " + name + ". One thing I would like for you to know about me is when I am not programing I love to " + hobby + ". I also want you to know that I am a " + adjective + " student who loves " + love + " but absolutely hates " + hate +
+# print to the screen a paragraph with the user variables in their respective places
+print("Hello, my name is " + name + ". One thing I would like for you to know about me is when I am not programing I love " + hobby + ". I also want you to know that I am a " + adjective + " student who loves " + love + " but absolutely hates " + hate +
       "! The best way to describe my personality is " + personalityTrait1 + " and " + personalityTrait2 + ". My personal philosophy or a strong belief that I have that guides me through my life is " + philosophy + ". I am excited to learn about programming!")
