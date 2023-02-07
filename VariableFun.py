93076898a259621c4652062452ff286710b94d1f
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-07 11:11:24 -0700

    practice examples

---
+++ VariableFun.py
@@ -0,0 +1,13 @@
+# int variable
+age = 50
+
+# float
+weight = 145.8
+
+# string
+name = "Trina Nixon"
+
+# boolean
+active = True
+
+# all data types are inferred, don't have to specify what they are
