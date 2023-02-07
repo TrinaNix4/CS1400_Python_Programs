3e32f41c3e2093b289712242d1749ca470bef65e
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-07 11:36:05 -0700

    coding example

---
+++ ArithmeticOperators.py
@@ -0,0 +1,19 @@
+answer = 7/3
+print(answer)
+
+# integer division
+answer = 7//3
+print(answer)
+
+# modulus
+answer = 7 % 3
+print(answer)
+
+# candy calculator - example
+students = int(input("How many students are in the class?"))
+pieces = int(input("how many pieces of candy per student?"))
+
+# calculation for total
+total = students * pieces
+
+print('You will need', total, 'pieces of candy.')
