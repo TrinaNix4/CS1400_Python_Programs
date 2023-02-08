7a1befda0eed5fc5a04830ca59a70df55ad7655e
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-08 10:58:06 -0700

    simple calculator practice

---
+++ SimpleCalculations.py
@@ -0,0 +1,7 @@
+
+# ask user for input, cast it as an int, and assigning it to variable 'repeatNum'
+repeatNum = int(input('Please enter a 2 digit number: '))
+
+# output
+result = repeatNum * (3 * 7 * 13 * 37)
+print('Watch it repeat!', result)
