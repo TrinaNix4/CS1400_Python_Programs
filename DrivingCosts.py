90928a00462bb2303df3d4fc55c6d1a706713be9
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-09 14:54:10 -0700

    fixed formatting

--- DrivingCosts.py
+++ DrivingCosts.py
@@ -2,14 +2,14 @@
 print("Welcome to the Driving Cost Calculator. I will calculate the cost of driving from Weber State University to 3 cities of your choice. Let's get started!")
 
 # user input
-carMilesPerGallon = input("How many miles per gallon does your car get?")
-currCostOfGas = input("What is the current cost of gas per gallon?")
+carMilesPerGallon = input("How many miles per gallon does your car get? \n")
+currCostOfGas = input("What is the current cost of gas per gallon? \n")
 
 # user input - first city
 city1 = input(
-    "Enter the first city you'd like to calculate the cost to drive to: ")
+    "Enter the first city you'd like to calculate the cost to drive to:  \n")
 milesToCity1 = int(input(
-    "How many miles is it from Weber State University to your first city?"))
+    "How many miles is it from Weber State University to your first city?  \n"))
 
 # calculation to determine cost - first city
 costToCity1 = (int(milesToCity1) /
@@ -19,9 +19,9 @@ print('The cost for driving to', city1, 'is $', round(costToCity1, 2))
 
 # user input - 2nd city
 city2 = input(
-    "Next, enter the second city you'd like to calculate the cost to drive to: ")
+    "Next, enter the second city you'd like to calculate the cost to drive to:  \n")
 milesToCity2 = int(input(
-    "How many miles is it from Weber State University to your second city?"))
+    "How many miles is it from Weber State University to your second city?  \n"))
 
 # calculation to determine cost - second city
 costToCity2 = (int(milesToCity2) /
@@ -31,9 +31,9 @@ print('The cost for driving to', city2, 'is $', round(costToCity2, 2))
 
 # user input - third city
 city3 = input(
-    "Last, enter the third city you'd like to calculate the cost to drive to: ")
+    "Last, enter the third city you'd like to calculate the cost to drive to:  \n")
 milesToCity3 = int(input(
-    "How many miles is it from Weber State University to your third city?"))
+    "How many miles is it from Weber State University to your third city?  \n"))
 
 # calculation to determine cost - third city
 costToCity3 = (int(milesToCity3) /
