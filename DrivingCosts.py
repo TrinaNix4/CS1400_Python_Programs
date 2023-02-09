fc09c1746fc53807d680f22a4b89770d17a6ab73
Trina Nixon <TrinaNixon7@gmail.com>
2023-02-09 13:12:49 -0700

    practice assignment driving costs

---
+++ DrivingCosts.py
@@ -0,0 +1,31 @@
+print("Welcome to the Driving Cost Calculator. I will calculate the cost of driving from Weber State University to 3 cities of your choice. Let's get started!")
+
+carMilesPerGallon = input("How many miles per gallon does your car get?")
+currCostOfGas = input("What is the current cost of gas per gallon?")
+
+city1 = input(
+    "Enter the first city you'd like to calculate the cost to drive to: ")
+milesToCity1 = input(
+    "How many miles is it from Weber State University to your first city?")
+costToCity1 = (int(milesToCity1) /
+               int(carMilesPerGallon)) * float(currCostOfGas)
+
+print('The cost for driving to', city1, 'is $', round(costToCity1, 2))
+
+city2 = input(
+    "Next, enter the second city you'd like to calculate the cost to drive to: ")
+milesToCity2 = input(
+    "How many miles is it from Weber State University to your second city?")
+costToCity2 = (int(milesToCity2) /
+               int(carMilesPerGallon)) * float(currCostOfGas)
+
+print('The cost for driving to', city2, 'is $', round(costToCity2, 2))
+
+city3 = input(
+    "Last, enter the third city you'd like to calculate the cost to drive to: ")
+milesToCity3 = input(
+    "How many miles is it from Weber State University to your third city?")
+costToCity3 = (int(milesToCity3) /
+               int(carMilesPerGallon)) * float(currCostOfGas)
+
+print('The cost for driving to', city3, 'is $', round(costToCity2, 2))
