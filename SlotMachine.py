eadd9befdbfa98f91cc7f302b36f987bbd23be7e
Trina Nixon <TrinaNixon7@gmail.com>
2023-03-06 20:30:40 -0700

    slot machine homework assignment

---
+++ SlotMachine.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+print("Trina's Slot Machine")
+tokens = int(
+    input("Please enter the number of tokens you want to start with: "))
+cashOut = False
+
+
+bet = int(input("How much would you like to bet? (enter 4 to cash out): "))
