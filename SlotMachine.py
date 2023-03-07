58babc0cd283abe7b05b8203a0d475bc0c3672fc
Trina Nixon <TrinaNixon7@gmail.com>
2023-03-06 23:17:37 -0700

    slot machine hw

--- SlotMachine.py
+++ SlotMachine.py
@@ -4,7 +4,24 @@ import random
 print("Trina's Slot Machine")
 tokens = int(
     input("Please enter the number of tokens you want to start with: "))
-cashOut = False
 
 
-bet = int(input("How much would you like to bet? (enter 4 to cash out): "))
+bet = int(input(
+    "You can bet 1, 2, or 3 tokens. How much would you like to bet? (enter 4 to cash out): "))
+while bet == 1 or bet == 2 or bet == 3:
+    # decrement tokens the amount of bet the player plays
+    tokens -= bet
+    slot1 = random.randint(1, 5)
+    slot2 = random.randint(1, 5)
+    slot3 = random.randint(1, 5)
+    print(slot1, slot2, slot3)
+
+    if slot1 == slot2 == slot3:
+        # the number they rolled to the power of their bet
+        winTokens = math.pow(slot1, bet)
+        # player wins tokens and the win is added to total
+        tokens += winTokens
+        print("You win ", winTokens, "tokens!")
+    else:
+        print("You lose ", bet, "tokens.")
+print("Tokens: ", tokens)
