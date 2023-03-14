020ddd89564ca4a1a7bc1b880c0a772811e89fdc
Trina Nixon <TrinaNixon7@gmail.com>
2023-03-14 11:06:48 -0600

    acronym maker assignment

---
+++ AcronymMaker.py
@@ -0,0 +1,7 @@
+# By Trina Nixon
+# Program will allow user to input a sentence. It will take the first letter of each word and create an acronym
+# Example: Random Access Memory: RAM
+# Example: Laugh Out Loud: LOL 
+
+print("Acronym Maker")
+
