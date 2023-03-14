7a34ec10ce2cb3e3bd4d59cdc986f46c5e5bccdb
Trina Nixon <TrinaNixon7@gmail.com>
2023-03-14 11:17:15 -0600

    acronym assignment

--- AcronymMaker.py
+++ AcronymMaker.py
@@ -5,3 +5,17 @@
 
 print("Acronym Maker")
 
+sentence = input("Enter a sentence, or q to quit: ")
+
+acronym = sentence[0]
+# so long as pos is greater or equal to 0, keep repeating this code 
+while pos >= 0:
+  #starts at pos instead of just the beginning
+    pos = sentence.find(" ", pos)
+    if pos >= 0:
+  # gets us to the character immediately after the space pos 
+      pos += 1
+      # add the letter onto our acronym
+      acronym += sentence[pos]
+  
+  print("Acronym: ", acronym)
