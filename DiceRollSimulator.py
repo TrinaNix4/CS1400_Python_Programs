f374b21126ee9ddfb08bf547ca3febd19b84c515
Trina Nixon <TrinaNixon7@gmail.com>
2023-03-23 12:07:52 -0600

    dice roll simulator program

---
+++ DiceRollSimulator.py
@@ -0,0 +1,44 @@
+# Program by Trina Nixon
+
+print("Welcome to my Dice Roll Simulator")
+
+# assignment parameters are as follows:
+# this program will simulate the rolling of two 6-sided dice
+# a dictionary will keep track of the # of time that each total # is thrown
+# dictionary values are as follows:
+# key = sum of the 2 dice
+# value = keep track of the # of rolls
+# if key does not exist, set the value as 1
+# if key does exist, add 1 to the value (like in dictionaries try it yourself assignment)
+
+# allow user to choose how many times the dice will be thrown
+# once dice thrown the specified number of times, print a histogram (using the * character) that shows the total percentage each number was rolled
+# each * will represent 1% of the total rolls
+
+# sample session:
+# Welcome to the dice throwing simulator!
+# How many dice rolls would you like to simulate? 1000
+
+# DICE ROLLING SIMULATION RESULTS
+# Each "*" represents 1 % of the total number of rolls.
+# Total number of rolls = 1000.
+# 2: ***
+# 3: ***
+# 4: ***********
+# 5: ***********
+# 6: ********
+# 7: ******************
+# 8: ****************
+# 9: **********
+# 10: *************
+# 11: *****
+# 12: **
+
+# Thank you for using the dice throwing simulator. Goodbye!
+
+# rubric requirements:
+# use of a dictionary
+# use of loops to iterate dictionary
+# use of random number generator
+
+#########################################################
